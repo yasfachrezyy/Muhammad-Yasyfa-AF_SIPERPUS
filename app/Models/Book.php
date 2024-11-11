0efe72b981e8fed0d2eaa7c468e920328847ec14
Dams168 <adamrohman2003@gmail.com>
2024-11-11 18:55:05 +0700

    add: Bab 6 Data Management

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'author',
+        'year',
+        'publisher',
+        'city',
+        'bookshelf_id',
+        'cover',
+    ];
+
+    public function bookshelf(): BelongsTo
+    {
+        return $this->belongsTo(Bookshelf::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,22 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    Route::get('/books', [BookController::class, 'index'])->name('books.index');
 });
 
-Route::group(['middleware' => ['role:pustakawan']], function () {
-    Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
-    Route::get('/books/edit/{id}', [BookController::class, 'edit'])->name('books.edit');
-    Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
-    Route::patch('/books/update', [BookController::class, 'update'])->name('books.update');
+Route::middleware('auth')->group(function () {
+    Route::get('/books', [BookController::class, 'index'])->name('book');
+    Route::get('/books/create', [BookController::class, 'create'])->name('book.create');
+    Route::post('/books', [BookController::class, 'store'])->name('book.store');
+    Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('book.edit');
+    Route::match(['put', 'patch'], '/books/{id}', [BookController::class, 'update'])->name('book.update');
+    Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('book.destroy');
 });
 
-require __DIR__.'/auth.php';
+// Route::group(['middleware' => ['role:pustakawan']], function () {
+//     Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+//     Route::get('/books/edit/{id}', [BookController::class, 'edit'])->name('books.edit');
+//     Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
+//     Route::patch('/books/update', [BookController::class, 'update'])->name('books.update');
+// });
+
+require __DIR__ . '/auth.php';
